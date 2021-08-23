21ec038386f5d3012c75153359349bd6c3f02360
Wiz Lee <wizlee.dev@gmail.com>
2021-08-24 01:46:22 +0800

    Completed POC for streaming video using LBRY API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,79 @@
 import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
-  `,
-  styles: []
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styles: [
+    `
+      input {
+        width: 20vw;
+        margin: 10px 10px 20px 10px;
+      }
+      label {
+        margin-left: 10px;
+      }
+      video {
+        width: 640px;
+        height: 360px;
+      }
+    `,
+  ],
 })
 export class AppComponent {
-  title = 'dashsee-poc';
+  baseURL: string = "https://api.lbry.tv/api/v1/proxy";
+  streamUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.streamUrl = "";
+  }
+
+  resolveUriAndSetStreamSrc(uri: string) {
+    if (uri) {
+      this.http
+        .post<any>(this.baseURL, {
+          method: "resolve",
+          params: { urls: uri },
+        })
+        .subscribe({
+          next: (data) => {
+            if (uri in data.result) {
+              const result = data.result[uri];
+              if ("error" in result) {
+                console.error(
+                  "Error when resolving URI!",
+                  result.error.text
+                );
+              }
+              else if (result?.value?.stream_type != "video") {
+                console.error(
+                  "URI don't resolve to a video!",
+                  result?.value?.stream_type
+                );
+              }
+              else {
+                const confirmedUri: string = result.short_url.substring(
+                  result.short_url.indexOf("lbry://")
+                );
+                this.http
+                  .post<any>(this.baseURL, {
+                    method: "get",
+                    params: {
+                      uri: confirmedUri,
+                      save_file: false,
+                    },
+                  })
+                  .subscribe((data) => {
+                    this.streamUrl = data?.result?.streaming_url;
+                    console.log("Stream URL = ", this.streamUrl);
+                  });
+              }
+            }
+          },
+          error: (error) => {
+            console.error("There was an error!", error);
+          },
+        });
+    }
+  }
 }
